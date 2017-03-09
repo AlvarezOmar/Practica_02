f236c5174a40031be16a32f4b2a24b5cabe0525f
AlvarezOmar <alvarezomar35@hotmail.com>
2017-03-08 23:13:11 -0600

    REGISTROS DE DATOS

---
+++ Practica_02/src/isc/isma4/figuras/Practica_2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isc.isma4.figuras;
+
+import isc.isma4.figuras.IFigura;
+
+/**
+ *
+ * @author Alvarez
+ */
+
+public class Practica_2 {
+    
+
+    public double getFigureArea(IFigura figure){
+        // marca el error por que hace falta clase y para eso debemos crear un enlace desde java application.
+      return figure.getArea();
+    }
+  
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
